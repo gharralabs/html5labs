d4fb6faabea3d820bf844dd4a113930e738b017b
Marine King <mkp@prime.co.kr>
2015-09-18 15:53:44 -0300

    match the card - inicial

---
+++ web/web/cardmatch/js/index.js
@@ -0,0 +1,8 @@
+﻿$().ready(function () {
+
+    $("#botao").click(function () {
+        $("#card1").addClass("mover-e-escalar");
+        $("#card2").addClass("rotacionar-direita");
+    });
+
+});
