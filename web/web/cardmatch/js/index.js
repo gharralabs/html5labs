7649cd77ac409d5d2d7eb9d9ecad5a626fab5a90
Marine King <mkp@prime.co.kr>
2015-09-18 23:26:15 -0300

    rotação de cartas

--- web/web/cardmatch/js/index.js
+++ web/web/cardmatch/js/index.js
@@ -1,8 +1,21 @@
 ﻿$().ready(function () {
+    
 
-    $("#botao").click(function () {
-        $("#card1").addClass("mover-e-escalar");
-        $("#card2").addClass("rotacionar-direita");
-    });
+    $("#cards").children()
+               .each(function (index) {
+                   $(this).click(function () {
+                       $(this).toggleClass("card-flipped");
+                   });
+               });
 
+
+    angulo = 0;
+    setInterval(function () {
+        angulo++;
+
+        
+
+        $("#teste").css('transform', 'rotateY(' + angulo + 'deg)');
+    }, 1000);
+    
 });
