400399650ad4d532e61686e2321c3a8afe5be97d
Marine King <mkp@prime.co.kr>
2015-09-20 01:17:54 -0300

    untangle - bola dourada

---
+++ web/web/untangle/untangle.js
@@ -0,0 +1,11 @@
+﻿$().ready(function () {
+
+    var canvas = document.getElementById("game");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "GOLD";
+    ctx.beginPath();
+    ctx.arc(100, 100, 50, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+
+});
